5c700d7222d6f496c66cf2b0e1fde73d6ad4e88e
Daniel Perez <doperez1997@gmail.com>
2023-10-28 15:37:03 -0600

    Removed the dev.danperez.gradle.scaler.dependencies package and it's contents

--- ScalerAndroidPlugin/src/main/kotlin/dev/danperez/gradle/ScalerExtension.kt
+++ ScalerAndroidPlugin/src/main/kotlin/dev/danperez/gradle/ScalerExtension.kt
@@ -1,20 +1,10 @@
 package dev.danperez.gradle
 
-import com.android.build.api.dsl.CommonExtension
-import com.squareup.anvil.plugin.AnvilExtension
-import dev.danperez.gradle.compose.configureComposeCompiler
-import dev.danperez.gradle.dependencies.ScalerDependencies
-import dev.danperez.gradle.handlers.AndroidFeaturesHandler
 import dev.danperez.gradle.handlers.AndroidHandler
-import dev.danperez.gradle.handlers.DaggerHandler
 import dev.danperez.gradle.handlers.FeaturesHandler
-import dev.danperez.gradle.util.setDisallowChanges
 import org.gradle.api.Action
-import org.gradle.api.GradleException
-import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalog
 import org.gradle.api.model.ObjectFactory
-import org.gradle.api.provider.Property
 import javax.inject.Inject
 
 @DslMarker
--- ScalerAndroidPlugin/src/main/kotlin/dev/danperez/gradle/dependencies/DependencyCollection.kt
+++
@@ -1,7 +0,0 @@
-package dev.danperez.gradle.dependencies
-
-public interface DependencyCollection {
-    public companion object {
-        internal const val GRADLE_PROPERTY_PREFIX = "scaler.dependencies."
-    }
-}
